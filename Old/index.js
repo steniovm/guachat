b81fff4fd0cac83a754473af3c84c80b9a01cbeb
steniovm <steniovm@gmail.com>
2023-10-13 14:31:32 -0300

    first commit GuaChat

---
+++ Old/index.js
@@ -0,0 +1,215 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+dotenv.config();
+const port = process.env.PORT || 80;
+
+//let messages = [];
+let guaxas = [];
+app.use(cors());
+app.use(express.static(path.join('public')));
+app.set('views',path.join('public'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/',(req,res)=>{
+    res.sendFile(join('index.html'));
+});
+
+function newGuaxa(user){
+    let result = true;
+    guaxas.forEach(el=>{
+        if (user.username === el.username){
+            result = false;
+        }
+    });
+    if (result){
+        user.gamers=[];
+        guaxas.push(user);
+    }
+    return result;
+}
+function newGamer(user){
+    let result = {guaxa:false, user:true, vaga:true, playnamber:0};
+    guaxas.forEach(el=>{
+        if(user.guaxaname === el.username){
+            if (el.gamers.length>=3){
+                result.vaga = false;
+            }else{
+                el.gamers.forEach(ele=>{
+                    if (user.username === ele.username){
+                        result.user = false;
+                    }
+                });
+            }
+            
+            if (result.user && result.vaga){
+                el.gamers.push(user);
+                result.playnamber = el.gamers.length;
+            }
+            result.guaxa = true;
+        }
+    });
+    return result;
+}
+
+function rolldices(atrr, nroll){
+	let result = {nums:[], words:[]};
+	let res = 0;
+    atrr = parseInt(atrr);
+	for(let i = 0; i< nroll; i++){
+		res = Math.ceil(Math.random()*6);
+		result.nums.push(res);
+		if (res < atrr){
+			result.words.push(' F ');
+		}else if (res > atrr){
+			result.words.push(' I ');
+		}else if (res === atrr){
+			result.words.push('Critico');
+		}
+	}
+	return result;
+}
+
+function testef(atrr, nroll){
+	let result = rolldices(atrr, nroll);
+	result.acertos = 0;
+	result.words.forEach(el => {
+		if(el == ' F ') {
+			result.acertos++;
+		}else if(el == 'Critico') {
+			result.acertos += 2;
+		}
+	});
+	return result;
+}
+
+function testei(atrr, nroll){
+	let result = rolldices(atrr, nroll);
+	result.acertos = 0;
+	result.words.forEach(el => {
+		if(el == ' I ') {
+			result.acertos++;
+		}else if(el == 'Critico') {
+			result.acertos += 2;
+		}
+	});
+	return result;
+}
+io.on('connection', function(socket){
+    let room = '';
+
+    console.log('usuario conectado com id:'+socket.id);
+
+    //socket.emit('previousMessages', messages);
+
+    socket.on('sendUser', async (data) =>{
+        //messages.push(data);
+        data.sid = socket.id;
+        //socket.broadcast.emit('receivedUser', data);
+        if (data.typeuser === "Guaxa"){
+            data.status = newGuaxa(data);
+            if (data.status){
+                room = data.username;
+                await socket.join(room);
+            }
+        }else{
+            data.status = newGamer(data);
+            if (data.status.guaxa && data.status.user && data.status.vaga && data.status.playnamber){
+                room = data.guaxaname;
+                await socket.join(room);
+            }
+        }
+        if (room){
+            guaxas.forEach(async (el)=>{
+                if (el.username===room){
+                    await io.to(room).emit('adduser', el);
+                    el.gamers.forEach(async (ele)=>{
+                        await io.to(room).emit('adduser', ele);
+                    });     
+                }
+            });
+        }
+        else{socket.emit('adduser', data);}
+    });
+
+    socket.on('sendMessage', data =>{
+        //messages.push(data);
+        io.to(room).emit('receivedMessage', data);
+    });
+
+    socket.on('rollDice', data=>{
+        let result;
+        if (data.typetest === 'o') result=rolldices(data.attr,data.nroll);
+        else if (data.typetest === 'f') result=testef(data.attr,data.nroll);
+        else if (data.typetest === 'i') result=testei(data.attr,data.nroll);
+        result.user = data.user;
+        io.to(data.room).emit('roolresult', result);
+    });
+
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId);
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    
+        socket.on('disconnect', () => {
+          socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        });
+      });
+
+
+    socket.on('disconnect', () => {
+        let index = -1;
+        let room = '';
+        guaxas.forEach((el,ind)=>{
+            if (el.sid === socket.id){
+                index = ind;
+                room = el.username;
+            }else{
+                let gindex = -1;
+                el.gamers.forEach((ele,inde)=>{
+                    if (ele.sid === socket.id){
+                        gindex = inde;
+                        room = ele.guaxaname;
+                    }
+                });
+                if (gindex>=0){
+                    el.gamers.splice(gindex,1);
+                }
+            }
+        });
+        if (index>=0){
+            guaxas.splice(index,1);
+        }
+        if (room){
+            guaxas.forEach(async (el)=>{
+                if (el.username===room){
+                    await io.to(room).emit('radduser', el);
+                    el.gamers.forEach(async (ele)=>{
+                        await io.to(room).emit('radduser', ele);
+                    });
+                    for(let i=el.gamers.length; i<3;i++){
+                        let datauser = {typeuser: 'Jogador', status:{guaxa:true, vaga:true, user:true, playnamber:i+1}};
+                        await io.to(room).emit('radduser', datauser);
+                    }
+                }
+            });
+        }
+        console.log('usuario desconectado id:'+socket.id);
+      });
+
+  });
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    server.listen(port, () =>{
+        console.log(`para conversar acesse: http://${add}:${port}`);
+        //console.log(err);
+        //console.log(fam);
+    });
+    return true;
+});
+
---
+++ public/script.js
@@ -0,0 +1,291 @@
+const chat = document.getElementById('chat');
+const submitbt = document.getElementById('submitbt');
+const username = document.getElementById('username');
+const message = document.getElementById('message');
+const messages = document.getElementById('messages');
+const modal = document.getElementById('modal');
+const modalform = document.getElementById('modalform');
+const inusername = document.getElementById('inusername');
+const typeguaxa = document.getElementById('typeguaxa');
+const typeplayer = document.getElementById('typeplayer');
+const caracterinfos = document.getElementById('caracterinfos');
+const guaxaname = document.getElementById('guaxaname');
+const caractername = document.getElementById('caractername');
+const attrlabel = document.getElementById('attrlabel');
+const caracteratrib = document.getElementById('caracteratrib');
+const submituser = document.getElementById('submituser');
+const showusername = document.getElementsByClassName('showusername');
+const caracterinfo = document.getElementsByClassName('caracterinfo');
+const atrr = document.getElementsByClassName('atrr');
+const caract = document.getElementsByClassName('caract');
+const dicebt = document.getElementsByClassName('dicebt');
+const mediabt = document.getElementsByClassName('mediabt');
+const mainusers = document.getElementById('mainusers');
+const showuserdiv = document.getElementsByClassName('showuserdiv');
+const PORT = 3000;
+const mediaopt = {video: true,audio: true};
+
+let datauser = {};
+let usernumber = 0;
+let localstream;
+let streamer;
+
+caracteratrib.value = Math.ceil(Math.random()*4)+1;
+attrlabel.innerHTML = caracteratrib.value;
+caracteratrib.addEventListener('change',()=>{
+    attrlabel.innerHTML = caracteratrib.value;
+});
+
+typeguaxa.addEventListener('click',()=>{
+    caracterinfos.classList.add('hiddemdiv');
+    guaxaname.required = false;
+    caractername.required = false;
+    caracteratrib.required = false;
+});
+typeplayer.addEventListener('click',()=>{
+    caracterinfos.classList.remove('hiddemdiv');
+    guaxaname.required = true;
+    caractername.required = true;
+    caracteratrib.required = true;
+});
+modalform.addEventListener('submit',(ev)=>{
+    ev.preventDefault();
+    let data = new FormData(modalform);
+    datauser = Object.fromEntries(data);
+    modal.classList.add('hiddemdiv');
+    username.value = data.get('username');
+    if (data.get('typeuser')==="Guaxa"){
+        usernumber = 0;
+    }else if (data.get('typeuser')==="Jogador"){
+        usernumber = 1;
+    }
+    renderMessage({author:"", message:"Bem Vindo ao Guaxinins e Gambiarras"});
+    socket.emit('sendUser', datauser);
+    //starmedia();
+});
+
+for(let i=0; i<dicebt.length; i++){
+    let rolldices = {};
+    rolldices.nroll = i%3 + 1;
+    if (i<3) {rolldices.typetest = 'o'}
+    else if (i<6) {rolldices.typetest = 'f'}
+    else {rolldices.typetest = 'i'}
+    dicebt[i].addEventListener('click',()=>{
+        rolldices.user = datauser.username;
+        rolldices.room = datauser.guaxaname || datauser.username;
+        rolldices.attr = datauser.caracteratrib;
+        socket.emit('rollDice', rolldices);
+        console.log(rolldices);
+    });
+}
+
+let socket = io();
+let author = '';
+let mess = '';
+
+function renderMessage(messag){
+    messages.innerHTML += `<div class="message"><strong>${messag.author}: </strong>${messag.message}</div>`;
+    messages.scrollTo({top: Array.from(messages.childNodes).reduce((acc, v) => acc + v.clientHeight, 0), behavior: "smooth"});
+}
+
+socket.on('previousMessages',(arr)=>{
+    arr.forEach(el => {
+        renderMessage(el);
+    });
+});
+
+socket.on('receivedMessage', (messag)=>{
+    renderMessage(messag);
+});
+/*
+socket.on('receivedUser', (messag)=>{
+    console.log(messag);
+});
+*/
+socket.on('adduser', (datauser)=>{
+    for(let i=0; i<showuserdiv.length; i++){
+        if (datauser.username == showuserdiv[i].querySelector('.showusername').innerHTML){
+            showuserdiv[i].remove();
+        }
+    }
+    if (datauser.typeuser === "Guaxa"){
+        if (datauser.status){
+            datauser.playnamber = 0;
+            datauser.caracteratrib = 0;
+            datauser.caractername = "Guaxa";
+            datauser.guaxaname = datauser.username;
+            showdatauser(datauser);
+            if(datauser.playnamber === usernumber)
+                renderMessage({author:'',message:`Você está conectado como ${datauser.username}, convide três amigos`});
+            videoconect(0);
+        }else{
+            renderMessage({author:'',message:'Já existe um guaxa online com este nome, tente outro.'});
+            modal.classList.remove('hiddemdiv');
+        }
+    }else{
+        if (!datauser.status.guaxa){
+            renderMessage({author:'',message:'Não existe um guaxa com este nome online, confirme com seu guaxa o nome de usuário dele.'});
+            modal.classList.remove('hiddemdiv');
+        }else if(!datauser.status.vaga){
+            renderMessage({author:'',message:'As vagas para jogar com este guaxa já estão preenchidas, aguarde um jogador sair ou tente com outro guaxa.'});
+            modal.classList.remove('hiddemdiv');
+        }else if(!datauser.status.user){
+            renderMessage({author:'',message:'Já existe um jogador com este nome jogando com este guaxa tente outro nome de usúario.'});
+            modal.classList.remove('hiddemdiv');
+        }else{
+            datauser.playnamber = datauser.status.playnamber;
+            renderMessage({author:'',message:`Bem vindo ao jogo ${datauser.username}`});
+            videoconect(datauser.playnamber);
+            showdatauser(datauser);
+        }
+    }
+});
+
+socket.on('removeuser', (numberuser)=>{
+    showuserdiv[numberuser].remove();
+});
+
+socket.on('radduser', (datauser)=>{
+    if (datauser.typeuser === "Guaxa"){
+        if (datauser.status){
+            datauser.playnamber = 0;
+            datauser.caracteratrib = 0;
+            datauser.caractername = "Guaxa";
+        }
+    }else{
+        datauser.playnamber = datauser.status.playnamber;
+        showdatauser(datauser);
+    }
+});
+
+socket.on('roolresult',(result)=>{
+    let str = [];
+    for(let i=0; i<result.nums.length; i++){
+        str.push(result.nums[i]+'-'+result.words[i]);
+    }
+    if(result.acertos || result.acertos==0)str.push("Acertos: "+result.acertos);
+    renderMessage({author:result.user,message:`Rolagem: ${str.join(', ')}`})
+});
+
+function showdatauser(datauser){
+    const newuser = `<div class="showuserdiv ${(datauser.typeuser === "Guaxa")?'divguaxa':''}">
+            <span class="showusername">${datauser.username || ""}</span>
+            <label class="caracterinfo ${(datauser.typeuser === "Guaxa")?'hiddemdiv':''}">
+                <strong class="atrr">${datauser.caracteratrib || ""}</strong>
+                <span class="caract">${datauser.caractername || ""}</span>
+            </label>
+            <video></video>
+        </div>`
+    mainusers.innerHTML += newuser;
+    /*
+    showusername[datauser.playnamber].innerHTML = datauser.username || "";
+    atrr[datauser.playnamber].innerHTML = datauser.caracteratrib || "";
+    caract[datauser.playnamber].innerHTML = datauser.caractername || "";
+    */
+}
+
+chat.addEventListener('submit',(ev)=>{
+    ev.preventDefault();
+    author = username.value;
+    mess = message.value;
+
+    if (author && mess){
+        let messageObject = {
+            'author': author,
+            'message': mess
+        }
+        //renderMessage(messageObject);
+        socket.emit('sendMessage', messageObject);
+    }
+    message.value = '';
+});
+
+for (let i=0; i<mediabt.length; i++){
+    mediabt[i].addEventListener('click',()=>{
+        mediabt[i].classList.toggle('borderoff');
+    });
+}
+
+
+//daqui pra baixo diz respeito ao streamer
+
+
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: PORT
+});
+const myVideo = document.getElementsByTagName('video')
+for(let i=0; i<myVideo.length; i++){
+    myVideo[i].muted = true
+}
+const peers = {}
+
+async function starmedia(userlocal=true){
+    let streamer
+    await navigator.mediaDevices.getUserMedia(mediaopt).then(stream => {
+        streamer = stream;
+    });
+    if(userlocal) {
+        localstream = streamer;
+        addVideoStream(myVideo[usernumber], localstream);
+    }
+    return streamer;
+}
+function stopmedias(){
+    localstream.getTracks().forEach(el=>el.stop());
+}
+async function videoconect(playnamber){
+    streamer = await starmedia(false);
+    streamer.usernumber = playnamber;
+    if (playnamber === usernumber) localstream = streamer;
+    addVideoStream(myVideo[playnamber], streamer);
+
+    myPeer.on('call', call => {
+        call.answer(streamer);
+        call.on('stream', userVideoStream => {
+        addVideoStream(myVideo[streamer.usernumber], userVideoStream);
+        console.log(userVideoStream);
+        });
+    });
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, streamer)
+    });
+}
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+});
+
+myPeer.on('open', id => {
+  let ROOM_ID = datauser.guaxaname+'V';
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  call.on('stream', userVideoStream => {
+    addVideoStream(myVideo[stream.usernumber], userVideoStream)
+  });
+  call.on('close', () => {
+    myVideo[stream.usernumber].remove();
+  });
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+}
+
+mediabt[0].addEventListener('click',()=>{
+    if (mediabt[0].classList.contains('borderoff'))stopmedias();
+    else starmedia();
+});
+
+mediabt[1].addEventListener('click',()=>{
+    if (mediabt[1].classList.contains('borderoff'))stopmedias();
+    else starmedia();
+});
